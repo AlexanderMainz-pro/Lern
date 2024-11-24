a304717b04b840bd963feab177357122e1773eea
AlexanderKhlus <68031957+AlexanderMainz-pro@users.noreply.github.com>
2024-11-24 22:30:36 +0300

    24.11
    24.11

--- OOP/iter and next.py
+++ OOP/iter and next.py
@@ -1,40 +1,50 @@
 class FRange:
-    def _init__(self, start=0.0, stop=0.0, step=0.5):
+    def __init__(self, start=0, stop=0, step=1):
         self.start = start
-        self.stop =  stop
+        self.stop = stop
         self.step = step
-
 
     def __iter__(self):
         self.value = self.start - self.step
         return self
 
-
     def __next__(self):
         if self.value + self.step < self.stop:
-            self.value += self.stop
+            self.value += self.step #увеличиваем на длину шага
             return self.value
         else:
             raise StopIteration
 
+<<<<<<< Updated upstream
 class FRange2D: #формирует списки
     def _init__(self, start=0.0, stop=0.0, step=0.5, rows=5):
+=======
+
+class FRange2D:#формирует списки
+    def __init__(self, start=0, stop=0, step=0, rows=5):
+>>>>>>> Stashed changes
         self.rows = rows
-        self.fr = FRange(start, stop, step)
+        self.fr = FRange(start, stop, step) #<--2
 
     def __iter__(self):
         self.value = 0
         return self
 
     def __next__(self):
-        self.value +=1
-        return iter (self.fr)#Возвращает итератор, в строках появ. значния
-
-fr = FRange(0, 2, 0.5, 4)
-for row in fr:
-    for x in row:
-        print(x)
+        if self.value < self.rows:
+             self.value +=1
+             return iter(self.fr)#Возвращает итератор, в строках появ. значния <--1
+        else:
+            raise StopIteration
 
 
+#fr = FRange(0,5,1)
+##for x in fr:
+#    print(x)
 
+fr2 =FRange2D(0, 5, 1)
+for row in fr2: #получаем каждый раз объект из 1-->
+    for x in row: #перебираем FRange и получаем конкретные значения 2-->
+        print(x, end=" ")
+    print()
 
