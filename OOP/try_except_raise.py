b8fa67ece6eb9bbb3c228e6dd691392d62fcd05e
AlexanderKhlus <khlys88@mail.ru>
2024-12-02 04:38:52 +0300

    2.12
    2.12

--- OOP/try_except_raise.py
+++ OOP/try_except_raise.py
@@ -1,3 +1,14 @@
+class ExceptionPrint(Exception):
+    pass #можем выстраивать свою иерархию исключений и работать с ними
+
+class ExpectionPrintSendData(ExceptionPrint):
+    def __init__(self, *args):
+        self.message = args[0] if args else None#
+
+    def __str__(self):
+        return f"сообщение {self.message}"# отображаем наше исключение ExpectionPrintSendData
+"""Класс при отправке данных принтеру"""
+
 class PrintData:# собственно печатает данные
     def print(self, data):
         self.send_data(data)
@@ -5,12 +16,16 @@ class PrintData:# собственно печатает данные
 
     def send_data(self, data):#отправляет данные на печать
         if not self.send_to_print(data):
-            raise Exception("принтер не отвечает")
+            raise ExpectionPrintSendData("принтер не отвечает") #можем отлавливать исключения
 
-    def send_to_print(self, data):#проверяет, может печатать данные или нет
+    def send_to_print(self, data): #проверяет, может печатать данные или нет
         return False
 
-p = PrintData
+p = PrintData()
 try:
     p.print("123")
-except
+except ExpectionPrintSendData: #можем отлавливать своё же исключение
+    print("Принтер не отвечает")
+except ExceptionPrint:
+    print("Общая ошибка печати")
+
