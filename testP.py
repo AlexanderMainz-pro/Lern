8c89c57e5d0255a17857bf0ee30b253515981d3a
AlexanderKhlus <khlys88@mail.ru>
2024-12-16 09:59:33 +0300

    16.12.2
    16.12.2

--- testP.py
+++ testP.py
@@ -1,7 +1,8 @@
-input_str = input()
-first_str, second_str = input_str.split()
-tri_str = first_str[:len(second_str)]
-index_str = tri_str[1::2]
-index_str2 = second_str[1::2]
-result = index_str == index_str
-print(result)
+# put your python code here
+x, y, z = map(str, input().split())
+a = x.ljust(4, "0")
+b = y.ljust(4, "0")
+c = z.ljust(4, "0")
+print(a)
+print(b)
+print(c)
