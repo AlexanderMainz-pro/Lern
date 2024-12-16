0122b3fe7ff646dcc5b0f6f7799f9818ff0d9585
AlexanderKhlus <khlys88@mail.ru>
2024-12-16 06:16:55 +0300

    16.12
    16.12

--- testP.py
+++ testP.py
@@ -1,3 +1,7 @@
-x = float(input())
-result = (0 <= x <= 2) or (10 <= x <= 20) + 0
+input_str = input()
+first_str, second_str = input_str.split()
+tri_str = first_str[:len(second_str)]
+index_str = tri_str[1::2]
+index_str2 = second_str[1::2]
+result = index_str == index_str
 print(result)
