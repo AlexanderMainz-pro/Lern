98958779ea42abcd3bd1bc82c3777df7fd0c67ee
AlexanderKhlus <khlys88@mail.ru>
2025-01-07 18:14:49 +0300

    07.01
    07.01

--- testX.py
+++ testX.py
@@ -1,20 +1,18 @@
-def road_display(z):
-    if z < 2:
-        return "No roads displayed"
-    elif 2 <= z < 5:
-        return "Roads fk=1 displayed (normal), Roads fk=7 displayed (pink)"
-    elif 5 <= z < 8:
-        return "Roads fk=1 displayed (purple), Roads fk=7 displayed (pink)"
-    elif 8 <= z < 9:
-        return "Roads fk=1 displayed (yellow), Roads fk=7 displayed (pink)"
-    elif 9 <= z < 12:
-        return "Roads fk=1 displayed (green), Roads fk=7 displayed (pink)"
-    elif 12 <= z < 15:
-        return "Roads fk=1 displayed (green, bold outline), Roads fk=9 displayed"
-    else:  # z ≥ 15
-        return "Roads fk=1 displayed (green, bold outline, translucent), Roads fk=9 displayed"
+class Variable:
+    def __init__(self, name, value):
+        self._name = name
+        self._value = value
 
-# Тестирование
-test_values = [-1, 0, 2, 4, 5, 8, 9, 12, 15, 20]
-for z in test_values:
-    print(f"z={z}: {road_display(z)}")
+    @property
+    def value(self):
+        print(self._name, 'GET', self._value)
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        print(self._name, 'SET', self._value)
+        self._value = value
+
+var_1 = Variable('var_1', 'val_1')
+var_2 = Variable('var_2', 'val_2')
+var_1.value, var_2.value = var_2.value, var_1.value
