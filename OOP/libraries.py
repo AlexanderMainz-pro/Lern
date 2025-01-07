305a87349a2d31e087ebee45fe2831cc5f94a148
AlexanderKhlus <khlys88@mail.ru>
2025-01-07 19:31:15 +0300

    07.01/2
    07.01/2

--- OOP/libraries.py
+++ OOP/libraries.py
@@ -5,3 +5,12 @@ from pprint import pprint #работа с файлами
 import os #библиотека операционки
 import tkinter #для создания приложений, front
 from dataclasses import dataclass #декоратор по созданию классов
+
+from functools import reduce
+#filter() принимает два аргумента – функцию-предикат и итерируемый объект. Он возвращает новый итератор,
+#содержащий только те элементы итерируемого объекта,
+#которые удовлетворяют условиям, заданным функцией-предикатом.
+
+#from memory_profiler import profile
+#Установите Memory Profiler с помощью pip: pip install memory-profiler
+#Используйте декоратор @profile перед функцией, которая может вызывать утечки памяти
