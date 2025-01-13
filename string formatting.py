47396383d205773fda7372e803ba7ea7270e0930
AlexanderKhlus <68031957+AlexanderMainz-pro@users.noreply.github.com>
2025-01-14 00:19:45 +0300

    14.01
    14.01

--- string formatting.py
+++ string formatting.py
@@ -4,5 +4,5 @@ print("my name  %s" % 'Ales')#устаревший способ конкатен
 #print('My name %(name)s, I am so old %s(yeas)s' % {'name': "Alex", 'yeas': 14})
 print('my name{}{}'.format('urban', '-university'))
 print('my name{0}{1}{0}'.format('urban', '-university'))
-print('my name{title}{postfix}{title}'.format(title='urban', postfix='-university')) #новый метод
+print('my name{title}{postfix}{title}'.format(title='urban', postfix='-university'))  #новый метод
 #Ф-строка
